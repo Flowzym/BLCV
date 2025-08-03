18c1574247a8af29ff427bd7111341e81a722691
Flowzym <hypophobia@gmail.com>
2025-08-03 19:47:15 +0200

    Updated mapBetterLetterWithTemplate.ts

--- src/modules/cv-designer/services/mapBetterLetterWithTemplate.ts
+++ src/modules/cv-designer/services/mapBetterLetterWithTemplate.ts
@@ -1,5 +1,5 @@
 import { LayoutElement } from "../types/section"
-import { getTemplateById } from "../../templates/predefinedTemplates"
+import { getTemplateById } from "../config/template_registry"
 
 export function mapBetterLetterToDesignerWithTemplate(cvData: any, templateId: string): LayoutElement[] {
   const template = getTemplateById(templateId)
