350b80f236ff357fc84a6fbb60f1666207341615
Flowzym <hypophobia@gmail.com>
2025-08-04 08:53:30 +0200

    Updated CVPreview.tsx

--- src/modules/cv-designer/components/CVPreview.tsx
+++ src/modules/cv-designer/components/CVPreview.tsx
@@ -14,6 +14,7 @@ import {
   getLayoutStats,
 } from "../services/layoutRenderer";
 import { RenderElementContent } from "../utils/renderElementContent";
+import { useStyleConfig } from "../context/StyleConfigContext";
 
 interface CVPreviewProps {
   sections?: LayoutElement[];
