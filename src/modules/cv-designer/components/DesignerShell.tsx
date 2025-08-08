1b681ab0f33bb7675f0740af0af8644d3e4f2cf8
Flowzym <hypophobia@gmail.com>
2025-08-08 23:03:22 +0200

    Updated DesignerShell.tsx

--- src/modules/cv-designer/components/DesignerShell.tsx
+++ src/modules/cv-designer/components/DesignerShell.tsx
@@ -1,4 +1,3 @@
-// src/modules/cv-designer/components/DesignerShell.tsx
 import React from "react";
 import CanvasToolbar from "./CanvasToolbar";
 import FabricCanvas from "../canvas/FabricCanvas";
@@ -6,16 +5,16 @@ import RightSidebar from "./RightSidebar";
 
 export default function DesignerShell() {
   return (
-    <div className="flex h-[calc(100vh-120px)] w-full">
-      <div className="flex min-w-0 flex-1 flex-col p-4">
-        <CanvasToolbar />
-        <div className="flex min-h-0 flex-1 overflow-auto rounded border bg-gray-50 p-6">
-          <div className="mx-auto">
+    <div className="flex flex-col h-full">
+      <CanvasToolbar />
+      <div className="flex min-h-0 flex-1">
+        <div className="flex-1 overflow-auto p-4">
+          <div className="inline-block shadow border bg-white">
             <FabricCanvas />
           </div>
         </div>
+        <RightSidebar />
       </div>
-      <RightSidebar />
     </div>
   );
 }
