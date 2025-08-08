a69f09db969a3f9d71396cca5e1d63dd510004af
Flowzym <hypophobia@gmail.com>
2025-08-08 16:25:09 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
+// src/main.tsx
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-// Global Styles
-import '@src/tokens.css';            // <-- liegt unter src/styles
+import './tokens.css';
 import './index.css';
 import 'react-quill/dist/quill.snow.css';
 
 const container = document.getElementById('root');
-
 if (container) {
   createRoot(container).render(
     <BrowserRouter>
