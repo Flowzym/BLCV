f6a04c45c17c29e1c837e1661390324c9909d69e
Flowzym <hypophobia@gmail.com>
2025-08-09 18:31:26 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-  css: {
-    // nutzt postcss.config.js
-  },
-  server: {
-    host: true,
-    port: 5173,
-  },
-});
+})
