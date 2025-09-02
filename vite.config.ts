a1c248f7f1b73c643fb3861d9f9d74c18e35cc6f
Flowzym <hypophobia@gmail.com>
2025-09-02 19:51:39 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/__tests__/setup.ts',
-  },
+  plugins: [react()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') }
+  }
 });
